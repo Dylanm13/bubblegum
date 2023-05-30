acf2445b5de2d44bc116968f9fa30f447d627b76
Dylanm13 <valesmoss@hotmail.fr>
2023-05-30 15:34:22 +0200

    changement logo

--- src/components/club/Club.jsx
+++ src/components/club/Club.jsx
@@ -8,16 +8,18 @@ import photo3 from '../../assets/logo_discord.png'
 import photo4 from '../../assets/logo_sponsorship.png'
 import photo5 from '../../assets/logo_discount.png'
 import photo6 from '../../assets/logo_event.png'
-import logo from '../../assets/logo_bbg.png'
+import logo from '../../assets/BBG_COLOR.png'
 
 const Club = () => {
   return (
     <div className='bgs__club'>
       <div className='bgs__club-container'>
-        <div className='bgs__club-container_banner'>
-            <img src={logo} alt='logo' className='img__logo'/>
-        </div>
-        <h1>Welcome to the Bubblegum Club</h1>
+          <div className='bgs__club-container_banner'>
+              <div className='bgs__club-container__image'>
+                <img src={logo} alt='logo' className='img__logo'/>
+              </div>
+          </div>
+        <h1>Welcome to Bubblegum Club, where music enthusiasts and aspiring artists come together to ignite their creativity and propel their careers to new heights. Our club offers a range of exceptional services that cater to every aspect of the music industry.</h1>
         <div className='bgs__club-container_text'>
             <div className='bgs__club-container_text--infos'>
                 <div className='bgs__club-container_text--infos_titles'>
@@ -25,7 +27,7 @@ const Club = () => {
                     <h2>Masterclasses</h2>
                 </div>
                 <div className='bgs__club-container_text--infos_advantages'>
-                    <p>We are going to organise masterclasses from 1 to 4 times a month where you will have the opportunity to learn from Jalal or other industry professionals on various topics related to the creative or business side of the industry. (Online and on location)</p>
+                    <p>We are going to organise masterclasses <strong>from 1 to 4 times a month</strong> where you will have the opportunity to learn from Jalal or other industry professionals on various topics related to the creative or business side of the industry. (Online and on location)</p>
                 </div>
             </div>
             <div className='bgs__club-container_text--infos'>
@@ -34,7 +36,7 @@ const Club = () => {
                     <h2>Sponsorships</h2>
                 </div>
                 <div className='bgs__club-container_text--infos_advantages'>
-                    <p>Being a member of the Bubblegum Club will allow you to sponsor your friends. If one of your friends books a session with us and says that it is thanks to you that he is here. You will get a 50€ discount on your next session.</p>
+                    <p>Being a member of the Bubblegum Club will allow you to sponsor your friends. If one of your friends books a session with us and says that it is thanks to you that he is here. You will <strong>get a 50€ discount on your next session</strong>.</p>
                 </div>
             </div>
             <div className='bgs__club-container_text--infos'>
@@ -43,7 +45,7 @@ const Club = () => {
                     <h2>Writer’s Camps</h2>
                 </div>
                 <div className='bgs__club-container_text--infos_advantages'>
-                    <p>If you are a songwriter, you will be thrilled to join our writer’s camps where you will learn more about songwriting with professionals. (Will occur once per trimester)</p>
+                    <p>If you are a songwriter, you will be thrilled to join our writer’s camps where you will <strong>learn more about songwriting with professionals</strong>. (Will occur once per trimester)</p>
                 </div>
             </div>
             <div className='bgs__club-container_text--infos'>
@@ -52,7 +54,7 @@ const Club = () => {
                     <h2>Discounts</h2>
                 </div>
                 <div className='bgs__club-container_text--infos_advantages'>
-                    <p>Joining the Club will give you a 10% discount on all your future bookings at the studio.</p>
+                    <p>Joining the Club will give you a <strong>10% discount</strong> on all your future bookings at the studio.</p>
                 </div>
             </div>
             <div className='bgs__club-container_text--infos'>
@@ -61,7 +63,7 @@ const Club = () => {
                     <h2>Discord Server</h2>
                 </div>
                 <div className='bgs__club-container_text--infos_advantages'>
-                    <p>With our Discord server you will be able to stay in touch with the Club. Hop on a call or chat with other members about the next event, get feedback on your songs, pitch your ideas, network etc…</p>
+                    <p>With our Discord server you will be able <strong>to stay in touch with the Club</strong>. Hop on a call or chat with other members about the next event, get feedback on your songs, pitch your ideas, network etc…</p>
                 </div>
             </div>
             <div className='bgs__club-container_text--infos'>
@@ -70,14 +72,16 @@ const Club = () => {
                     <h2>Events</h2>
                 </div>
                 <div className='bgs__club-container_text--infos_advantages'>
-                    <p>Once a week we are going to organise events with all club members where you will be able to access all studios for 3 hours, network with other artists, get feedback on your songs and enjoy our free drinks.</p>
+                    <p>Once a week we are going to organise events with all club members where you will be able to <strong>access all studios for 3 hours</strong>, network with other artists, get feedback on your songs and enjoy our free drinks.</p>
                 </div>
             </div>
         </div>
         <div className='bgs__club-container_top'>
             <button className='button_gradient'><a href='https://docs.google.com/forms/d/e/1FAIpQLSc3p3sqwzwZDoA4ymcy7EagHj7dkx2DZHhrVA-Z1DD3drx8AA/viewform' target='_blank' rel="noreferrer noopener">APPLY NOW</a></button>
         </div>
-        <div className='bgs__club-container_footer'>Important Notice: Please be informed that in order to become a member of the Club, there is a requirement to settle the Club's annual fees, amounting to 100€. Additionally, to proceed with your Club Application, kindly click on the "APPLY" button.</div>
+        <div className='bgs__club-container_footer'>
+            <p>Important Notice: Please be informed that in order to become a member of the Club, there is a <strong>requirement to settle the Club's annual fees, amounting to 100€</strong>. Additionally, to proceed with your Club Application, kindly click on the "APPLY" button.</p>
+        </div>
       </div>
     </div>
   )
